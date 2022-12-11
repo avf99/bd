0a2ca5c1fa04411601d39521e6f22e5ed92f7af9
avf99 <90771483+avf99@users.noreply.github.com>
2022-12-11 23:47:23 +0330

    Add files via upload

--- BD/code.js
+++ BD/code.js
@@ -4,5 +4,31 @@ const root = document.querySelector(':root');
 var crabSize = Math.min(vh * 0.3, vw * 0.3);
 var sunDiameter = Math.min(vh * 0.12, vw * 0.12);
 // set css variable
-root.style.setProperty('--crab-size', crabSize + "px");
-root.style.setProperty('--sun-diameter', sunDiameter + "px");
+
+function getChromeVersion () {     
+    var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+
+    return raw ? parseInt(raw[2], 10) : false;
+}
+
+if(getChromeVersion() <= 70) {
+    root.style.setProperty('--crab-size', crabSize + "px");
+    root.style.setProperty('--sun-diameter', sunDiameter + "px");
+}
+
+function blowOut() {
+    var smokes = document.getElementsByClassName("smoke");
+    var flame = document.getElementsByClassName("flame");
+    
+    for (let i = 0; i < flame.length; i++) {
+        flame[i].style.opacity = 0;
+    }
+    
+    for (let i = 0; i < smokes.length; i++) {
+        smokes[i].style.animation = "flame-off 3s linear forwards";
+        smokes[i].style.animationDelay = i%5 * 0.5 + "s";
+    }
+}
+
+
+
