dc4c4e60551e9aa748f482cb5f428fb3fb790870
avf99 <90771483+avf99@users.noreply.github.com>
2022-12-14 18:52:05 +0330

    Add files via upload

--- BD/code.js
+++ BD/code.js
@@ -1,6 +1,6 @@
 var vh = window.innerHeight;
 var vw = window.innerWidth;
-const root = document.querySelector(':root');
+var root = document.querySelector(':root');
 var crabSize = Math.min(vh * 0.3, vw * 0.3);
 var sunDiameter = Math.min(vh * 0.12, vw * 0.12);
 // set css variable
@@ -28,7 +28,37 @@ function blowOut() {
         smokes[i].style.animation = "flame-off 3s linear forwards";
         smokes[i].style.animationDelay = i%5 * 0.5 + "s";
     }
+    
+    var button = document.getElementById("blowout");
+    var firstP = document.getElementById("firstP");
+    var secondP = document.getElementById("secondP");
+    var thirdP = document.getElementById("thirdP");
+    
+    firstP.innerHTML = "The audio is not necesseary";
+    secondP.innerHTML = "Attention:";
+    secondP.style.color = "#dd0000"
+    thirdP.innerHTML = "( Headphones needed )";
+    button.innerHTML = "Play Audio";
+    button.setAttribute("onClick", "playAudio()");
+    
 }
+
+function playAudio() {
+    var congrats = document.getElementById("congrats");
+    
+    congrats.play();
+}
+
+            function handleLoading() {
+                let preload = document.getElementById("preload");
+                let mainPage = document.getElementById("main");
+                
+                preload.style.display = "none";
+                mainPage.style.display = "block";
+                console.log("hey")
+            }
+            
+            window.onload = handleLoading()
 
 
 
