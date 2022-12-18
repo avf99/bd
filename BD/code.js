631e3ad65c75f914c3edc990e19e9391f0a186b7
avf99 <90771483+avf99@users.noreply.github.com>
2022-12-18 18:07:25 +0330

    Add files via upload

--- BD/code.js
+++ BD/code.js
@@ -17,16 +17,36 @@ if(getChromeVersion() <= 70) {
 }
 
 function blowOut() {
-    var smokes = document.getElementsByClassName("smoke");
+    let rightCandle = document.getElementById("rightCandle");
+    let leftCandle = document.getElementById("leftCandle");
+    for (let i = 0; i < 50; i++) {
+        smokeRight = document.createElement("div");
+        rightCandle.appendChild(smokeRight);
+        smokeRight.className = "smoke-right smoke"
+        
+        smokeLeft = document.createElement("div");
+        leftCandle.appendChild(smokeLeft);
+        smokeLeft.className = "smoke-left smoke"
+    }
+    
+    var smokesLeft = document.getElementsByClassName("smoke-left");
+    var smokesRight = document.getElementsByClassName("smoke-right");
     var flame = document.getElementsByClassName("flame");
     
     for (let i = 0; i < flame.length; i++) {
         flame[i].style.opacity = 0;
     }
     
-    for (let i = 0; i < smokes.length; i++) {
-        smokes[i].style.animation = "flame-off 3s linear forwards";
-        smokes[i].style.animationDelay = i%5 * 0.5 + "s";
+    if (smokesLeft.length == 50) {
+        for (let i = 0; i < smokesLeft.length; i++) {
+            smokesLeft[i].style.animation = "flame-off-left 3s linear forwards";
+            smokesLeft[i].style.animationDelay = i%50 * 0.2 + "s";
+        }
+    
+        for (let i = 0; i < smokesRight.length; i++) {
+            smokesRight[i].style.animation = "flame-off-right 3s linear forwards";
+            smokesRight[i].style.animationDelay = i%50 * 0.2 + "s";
+        }
     }
     
     var button = document.getElementById("blowout");
